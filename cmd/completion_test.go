e543eccde9c15fe189d8eb53e30f99834acff6b8
xztaityozx <taityonohibi@gmail.com>
2020-10-29 20:59:50 +0900

    補完用のスクリプトを出力する --completionオプションの追加 (#11)
    * add --complete
    
    * rename paser to parser

---
+++ cmd/completion_test.go
@@ -0,0 +1,69 @@
+package cmd
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCompletion_Gen(t *testing.T) {
+	as := assert.New(t)
+
+	dict := map[string]func() []byte{
+		"zsh": func() []byte {
+			var data []byte
+			buf := bytes.NewBuffer(data)
+			_ = rootCmd.GenZshCompletion(buf)
+
+			return data
+		},
+		"bash": func() []byte {
+			var data []byte
+			buf := bytes.NewBuffer(data)
+			_ = rootCmd.GenBashCompletion(buf)
+
+			return data
+		},
+		"fish": func() []byte {
+			var data []byte
+			buf := bytes.NewBuffer(data)
+			_ = rootCmd.GenFishCompletion(buf, true)
+
+			return data
+		},
+		"pwsh": func() []byte {
+			var data []byte
+			buf := bytes.NewBuffer(data)
+			_ = rootCmd.GenPowerShellCompletion(buf)
+
+			return data
+		},
+		"PowerShell": func() []byte {
+			var data []byte
+			buf := bytes.NewBuffer(data)
+			_ = rootCmd.GenPowerShellCompletion(buf)
+
+			return data
+		},
+	}
+
+	for shell, expect := range dict {
+		t.Run(shell, func(t *testing.T) {
+			var data []byte
+			buf := bytes.NewBuffer(data)
+			err := Completion{W: buf}.Gen(rootCmd, shell)
+
+			as.Nil(err)
+			as.Equalf(expect(), data, "shell: %s", shell)
+		})
+	}
+
+	t.Run("invalid shell name", func(t *testing.T) {
+		var data []byte
+		buf := bytes.NewBuffer(data)
+		err := Completion{W: buf}.Gen(rootCmd, "")
+
+		as.Error(err)
+	})
+}
--- parser/parser.go
+++ parser/parser.go
@@ -1,4 +1,4 @@
-package paser
+package parser
 
 import (
 	"fmt"
