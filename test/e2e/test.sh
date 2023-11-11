5aea19c7de1099946fc20c324fdd7fff72760503
xztaityozx <taityonohibi@gmail.com>
2023-11-11 23:41:35 +0900

    test: E2Eテストを追加

---
+++ test/e2e/test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -ue
+
+SEL=../../dist/sel
+
+ls -1 | grep -v test.sh | \
+  while read DIR; do
+    commandline=$(cat ./$DIR/commandline)
+    input=$DIR/input
+    output=$DIR/output
+    echo -en "test $DIR: $SEL $commandline ... "
+    cat $input | eval "$SEL $commandline" | diff - $output && echo "OK" || echo "NG"
+  done
