59211a436e6d6ee18eeb518f785823fa8f215259
xztaityozx <taityonohibi@gmail.com>
2023-12-02 00:33:08 +0900

    feat: pwshでのテストにPesterを使う

--- test/e2e/test.ps1
+++ test/e2e/test.ps1
@@ -1,20 +1,22 @@
-$result = Get-ChildItem 0* | ForEach-Object {
+Import-Module -Name Pester -PassThru
+
+$tests = Get-ChildItem '0*' | ForEach-Object {
   $name=$_.Name;
+  $inputFile="$name/input";
+  $outputFile="$name/output";
   $arguments=(Get-Content "$name/commandline");
-  $command="Get-Content $name/input | ../../../dist/sel $arguments";
+  
+  @{ 
+    Name = $name;
+    InputFile = $inputFile;
+    OutputFile = $outputFile;
+    Arguments = $arguments;
+  };
+}
 
-  Compare-Object (Invoke-Expression "$command") (Get-Content "$name/output");
-  if ( $? -eq $True ) {
-    Write-Host "${name}: sel ${arguments} ... OK";
-    0;
-  } else {
-    Write-Host "${name}: sel ${arguments} ... NG";
-    1;
+Describe 'sel on PowerShell' {
+  It "cat <inputFile> | sel <arguments> Returns <outputFile> (<name>)" -ForEach $tests {
+    $expected = Get-Content $outputFile;
+    Invoke-Expression "Get-Content $inputFile | ../../../dist/sel $arguments" | Should -Be $expected;
   }
-} | Measure-Object -Sum
-
-if ($result.Sum -eq 0) {
-  exit 0
-} else {
-  exit 1
 }
