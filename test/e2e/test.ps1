8137eb74e72c16bea5ab017b14c5473c32723153
xztaityozx <taityonohibi@gmail.com>
2023-12-02 12:17:57 +0900

    feat: OSにあった拡張子を出力できるようにmakeを変更

--- test/e2e/test.ps1
+++ test/e2e/test.ps1
@@ -17,6 +17,6 @@ $tests = Get-ChildItem '0*' | ForEach-Object {
 Describe 'sel on PowerShell' {
   It "sel -f <inputFile> <arguments> Returns <outputFile> (<name>)" -ForEach $tests {
     $expected = Get-Content -Encoding utf8 $outputFile;
-    Invoke-Expression "../../../dist/sel -f $inputFile $arguments" | Should -Be $expected;
+    Invoke-Expression "Get-Content -Encoding utf8 $inputFile | ../../../dist/sel.exe $arguments" | Should -Be $expected;
   }
 }
