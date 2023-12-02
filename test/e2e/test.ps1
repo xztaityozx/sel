8bc546aa36008efae8343fcbe420a072da1b7263
xztaityozx <taityonohibi@gmail.com>
2023-12-02 09:03:03 +0900

    test: stdinからではなく -f オプション経由で入力するようにしてみる

--- test/e2e/test.ps1
+++ test/e2e/test.ps1
@@ -15,8 +15,8 @@ $tests = Get-ChildItem '0*' | ForEach-Object {
 }
 
 Describe 'sel on PowerShell' {
-  It "cat <inputFile> | sel <arguments> Returns <outputFile> (<name>)" -ForEach $tests {
+  It "sel -f <inputFile> <arguments> Returns <outputFile> (<name>)" -ForEach $tests {
     $expected = Get-Content -Encoding utf8 $outputFile;
-    Invoke-Expression "Get-Content -Encoding utf8 $inputFile | ../../../dist/sel $arguments" | Should -Be $expected;
+    Invoke-Expression "../../../dist/sel -f $inputFile $arguments" | Should -Be $expected;
   }
 }
