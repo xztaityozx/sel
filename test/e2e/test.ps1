58e1efc895c667c4d127d7f4f18b6bf18268ee4a
xztaityozx <taityonohibi@gmail.com>
2023-12-02 12:22:04 +0900

    feat: パスを自動で解決できるようにする

--- test/e2e/test.ps1
+++ test/e2e/test.ps1
@@ -14,9 +14,15 @@ $tests = Get-ChildItem '0*' | ForEach-Object {
   };
 }
 
+$selPath = if ( [System.IO.Path]::Exists("../../../dist/sel.exe") ) {
+  "../../../dist/sel.exe"
+} else {
+  "../../../dist/sel"
+}
+
 Describe 'sel on PowerShell' {
   It "sel -f <inputFile> <arguments> Returns <outputFile> (<name>)" -ForEach $tests {
     $expected = Get-Content -Encoding utf8 $outputFile;
-    Invoke-Expression "Get-Content -Encoding utf8 $inputFile | ../../../dist/sel.exe $arguments" | Should -Be $expected;
+    Invoke-Expression "Get-Content -Encoding utf8 $inputFile | $selPath $arguments" | Should -Be $expected;
   }
 }
